432e52f7e1c11a039fdb5289f6044262442b64b9
Tari-dev <tariqmuhammmad02@gmail.com>
2024-08-10 20:58:20 +0530

    fix few bugs

--- popup.js
+++ popup.js
@@ -5,13 +5,13 @@ if (typeof browser === "undefined") {
 
 document.getElementById("addThisPage").addEventListener("click", function () {
   browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-      const url = encodeURIComponent(tabs[0].url);
-      browser.windows.create({
-          url: `https://rinkoe.com/ext/additem?url=${url}`,
-          type: "popup",
-          width: 500,
-          height: 700
-      });
+    const url = encodeURIComponent(tabs[0].url);
+    browser.windows.create({
+      url: `https://rinkoe.com/ext/additem?url=${url}`,
+      type: "popup",
+      width: 500,
+      height: 700
+    });
   });
 });
 
@@ -21,19 +21,21 @@ document.getElementById("addCustomItem").addEventListener("click", function () {
 
   // Check if the link input is not empty
   if (customLink !== "" && (customLinkInput.value.startsWith("http://") || customLinkInput.value.startsWith("https://"))) {
-      browser.windows.create({
-          url: `https://rinkoe.com/ext/additem?url=${customLink}`,
-          type: "popup",
-          width: 500,
-          height: 700
+    browser.windows.create({
+      url: `https://rinkoe.com/ext/additem?url=${customLink}`,
+      type: "popup",
+      width: 500,
+      height: 700
+    });
   } else {
-      // Change the border color to red if the link is empty
-      customLinkInput.style.borderColor = "red";
+    // Change the border color to red if the link is empty
+    customLinkInput.style.borderColor = "red";
 
-      // You can also display an error message or take additional actions
-      console.log("Link input is empty. Please enter a link.");
+    // You can also display an error message or take additional actions
+    console.log("Link input is empty. Please enter a link.");
   }
 });
+
 
 // Reset the border color when the user starts typing
 document.getElementById("customLink").addEventListener("input", function () {
